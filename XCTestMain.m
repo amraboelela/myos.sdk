27551a68f9ec9ace8b59f04204a879326b74ef5d
Amr Aboelela <amraboelela@gmail.com>
2016-05-26 15:23:04 -0700

    XCTestMain.m

--- XCTestMain.m
+++ XCTestMain.m
@@ -5,16 +5,12 @@
 //  Copyright © 2016 Ayesha. All rights reserved.
 //
 
-//#import <RengoFoundation/RengoFoundation.h>
 #import <XCTest/XCTest.h>
-//#import "LevelDBTests.h"
 
 int main(void)
 {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-//    DLog();
-    
-    
+
     int numberOfClasses = objc_getClassList(NULL, 0);
     Class *classList = malloc(numberOfClasses * sizeof(Class));
     numberOfClasses = objc_getClassList(classList, numberOfClasses);
@@ -25,25 +21,15 @@ int main(void)
         NSString *className = NSStringFromClass(class);
         //NSLog(@"%@", className);
         if ([className rangeOfString:@"Tests"].length > 0) {
-            //if (-1 > 0) {
-            //Class classFromName = NSClassFromString(className);
-            //NSLog(@"%d", [className rangeOfString:@"Tests"].location);
             id classInstance = [[[class alloc] init] autorelease];
             if ([classInstance isKindOfClass:[XCTestCase class]]) {
                 NSLog(@"[class isKindOfClass:XCTestCase]");
-            
-                //class *testsClass = [[[class alloc] init] autorelease];
+
                 [classInstance runTest];
             }
         }
     }
     free(classList);
-    
-    /*
-    LevelDBTests *levelDBTests = [[[LevelDBTests alloc] init] autorelease];
-    [levelDBTests runTest];*/
-    //testClasses = IOPipeRunCommand(@"echo ${OBJECTS}", YES);
-    //DLog(@"testClasses: %@", testClasses);
     [pool release];
     return 0;
 }
