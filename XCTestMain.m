d6a581fa0f6cb0095f2bbf285ed18b7c5074097a
Amr Aboelela <amraboelela@gmail.com>
2016-05-26 16:04:12 -0700

    XCTestMain.m

--- XCTestMain.m
+++ XCTestMain.m
@@ -14,18 +14,15 @@ int main(void)
     int numberOfClasses = objc_getClassList(NULL, 0);
     Class *classList = malloc(numberOfClasses * sizeof(Class));
     numberOfClasses = objc_getClassList(classList, numberOfClasses);
-    
     for (int idx = 0; idx < numberOfClasses; idx++) {
         Class class = classList[idx];
         NSString *className = NSStringFromClass(class);
         if ([className rangeOfString:@"Tests"].length > 0) {
             id classInstance = [[[class alloc] init] autorelease];
             if ([classInstance isKindOfClass:[XCTestCase class]]) {
-                //NSDateFormatter *dateFormatter=[[NSDateFormatter alloc] init];
-                //[dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
-                NSLog(@"Test suite '%@' started", className);
+                //NSLog(@"Test Suite '%@' started.", className);
                 [classInstance runTest];
-                NSLog(@"Test suite '%@' passed", className);
+                //NSLog(@"Test Suite '%@' passed.", className);
             }
         }
     }
