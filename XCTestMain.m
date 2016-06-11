742c4d5fe986385b2b4d48d680f4ce30491ff49c
Amr Aboelela <amraboelela@gmail.com>
2016-06-11 16:59:50 -0700

    NSLog(@"     Executed %d test%@, with %d failure%@ in %0.3f seconds", testCount, (testCount > 1) ? @"s" : @"",  failureCount, (failureCount > 1)?@"s":@"", [NSDate timeIntervalSinceReferenceDate] - currentTime);

--- XCTestMain.m
+++ XCTestMain.m
@@ -14,21 +14,29 @@ int main(void)
     int numberOfClasses = objc_getClassList(NULL, 0);
     Class *classList = malloc(numberOfClasses * sizeof(Class));
     numberOfClasses = objc_getClassList(classList, numberOfClasses);
-    
+    int failureCount = 0;
+    int testCount = 0;
+    NSTimeInterval currentTime = [NSDate timeIntervalSinceReferenceDate];
     for (int idx = 0; idx < numberOfClasses; idx++) {
         Class class = classList[idx];
         NSString *className = NSStringFromClass(class);
         if ([className rangeOfString:@"Tests"].length > 0) {
             id classInstance = [[[class alloc] init] autorelease];
             if ([classInstance isKindOfClass:[XCTestCase class]]) {
-                //NSDateFormatter *dateFormatter=[[NSDateFormatter alloc] init];
-                //[dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
                 NSLog(@"Test suite '%@' started", className);
                 [classInstance runTest];
                 NSLog(@"Test suite '%@' passed", className);
+                failureCount += _failureCount;
+                testCount += _testCount;
             }
         }
     }
+    if (failureCount > 0) {
+        NSLog(@"Test Suite 'All tests' failed.");
+    } else {
+        NSLog(@"Test Suite 'All tests' passed.");
+    }
+    NSLog(@"     Executed %d test%@, with %d failure%@ in %0.3f seconds", testCount, (testCount > 1) ? @"s" : @"",  failureCount, (failureCount > 1)?@"s":@"", [NSDate timeIntervalSinceReferenceDate] - currentTime);
     free(classList);
     [pool release];
     return 0;
